51062efd1b5e944498744542c6ab713455726785
sayali-redasani <sayaliredasani@gmail.com>
2022-03-08 18:52:48 +0530

    task

---
+++ App.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import DisplayTasks from './screens/DisplayTasks';
+import Task from './screens/task';
+import firebase from "firebase"
+import { firebaseConfig } from "./config";
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="DisplayTasks" component={DisplayTasks} />
+        <Stack.Screen name="Task" component={Task} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
